d1b1cbced1e548f55b14ccfd37c1d22284d8e0f7
Wilfred van der Deijl <wilfred@vanderdeijl.com>
2017-12-28 14:10:20 +0100

    moved firebase and appengine directories
    so we have directories per target platform

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "deploy go bom stripper"
-(cd bom-stripper && gcloud app deploy --quiet --project future-app-backend)
+(cd appengine && gcloud app deploy --quiet --project future-app-backend)
 
 echo "deploy firebase"
-(cd functions && npm i && npm run deploy)
+(cd firebase/functions && npm i && npm run deploy)
